5a8e7da7fc7180971256bd8743f26b79a85246bb
Thiago <thiago8jesusaraujo@gmail.com>
2024-12-23 12:16:19 -0300

    Operação de instanciação da base de dados

---
+++ src/main/java/com/thiagodejesus/workshopmongo/config/Instantiation.java
@@ -0,0 +1,31 @@
+package com.thiagodejesus.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.thiagodejesus.workshopmongo.domain.User;
+import com.thiagodejesus.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner{
+	
+	@Autowired
+	UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+	}
+
+}
