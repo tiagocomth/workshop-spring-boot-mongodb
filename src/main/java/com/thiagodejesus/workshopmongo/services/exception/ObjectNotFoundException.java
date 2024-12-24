fb808d17e2b0e1c9aa286cfde73dd583424e9c3e
Thiago <thiago8jesusaraujo@gmail.com>
2024-12-24 11:28:35 -0300

    Obtendo um usuário por id

---
+++ src/main/java/com/thiagodejesus/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,12 @@
+package com.thiagodejesus.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+	
+	
+}
