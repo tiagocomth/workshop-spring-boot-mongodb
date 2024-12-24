03327c771ad2c7af5457045261c88aa05ef2e31d
Thiago <thiago8jesusaraujo@gmail.com>
2024-12-24 18:06:18 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/thiagodejesus/workshopmongo/resources/UserResource.java
+++ src/main/java/com/thiagodejesus/workshopmongo/resources/UserResource.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
