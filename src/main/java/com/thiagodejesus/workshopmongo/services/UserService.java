3052ef1ddfb52347fda0a3b1e04676aa44c98611
Thiago <thiago8jesusaraujo@gmail.com>
2024-12-24 12:02:31 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/thiagodejesus/workshopmongo/services/UserService.java
+++ src/main/java/com/thiagodejesus/workshopmongo/services/UserService.java
@@ -33,4 +33,9 @@ public class UserService {
 	public User fromDto(UserDTO obj) {
 		return new User(obj.getId(), obj.getName(), obj.getEmail());
 	}
+	
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
 }
