056f2e0c45d4f0998a2a782430dfe7a7671af0b9
Thiago <thiago8jesusaraujo@gmail.com>
2024-12-24 11:48:39 -0300

    Inserção de usuário com POST

--- src/main/java/com/thiagodejesus/workshopmongo/services/UserService.java
+++ src/main/java/com/thiagodejesus/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.thiagodejesus.workshopmongo.domain.User;
+import com.thiagodejesus.workshopmongo.dto.UserDTO;
 import com.thiagodejesus.workshopmongo.repository.UserRepository;
 import com.thiagodejesus.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -15,12 +16,21 @@ public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
 	}
+
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
-		}
+	}
+	
+	public User insert(User user) {
+		return repo.insert(user);
+	}
+	
+	public User fromDto(UserDTO obj) {
+		return new User(obj.getId(), obj.getName(), obj.getEmail());
+	}
 }
