e392ecde171870d7a7eea260528844d8c01efb74
Thiago <thiago8jesusaraujo@gmail.com>
2024-12-25 10:05:14 -0300

    Consulta simples com query methods

--- src/main/java/com/thiagodejesus/workshopmongo/services/PostService.java
+++ src/main/java/com/thiagodejesus/workshopmongo/services/PostService.java
@@ -1,12 +1,12 @@
 package com.thiagodejesus.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.thiagodejesus.workshopmongo.domain.Post;
-import com.thiagodejesus.workshopmongo.domain.User;
 import com.thiagodejesus.workshopmongo.repository.PostRepository;
 import com.thiagodejesus.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -19,6 +19,10 @@ public class PostService {
 	public Post findById(String id) {
 		Optional<Post> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
 	}
 	
 }
